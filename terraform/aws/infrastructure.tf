672b9de631da88a457d9ed7760c612910736fd95
Nkiruka Judith Obilor <78522812+Nikiobilor@users.noreply.github.com>
2026-05-06 14:37:16 +0100

    updated infrastructure.tf file,replace skip_final_snapshot from false to true

--- terraform/aws/infrastructure.tf
+++ terraform/aws/infrastructure.tf
@@ -245,7 +245,7 @@ resource "aws_db_instance" "main" {
   password               = var.mysql_password
   db_subnet_group_name   = aws_db_subnet_group.main.name
   vpc_security_group_ids = [aws_security_group.rds.id]
-  skip_final_snapshot    = false
+  skip_final_snapshot    = true
   multi_az               = true    # Automatic failover for high availability
   backup_retention_period = 7
 }
